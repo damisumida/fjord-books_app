0c6490a09fd50b26a6b8f8c55621574e42dcd280
damisumida <gjptmda628@gmail.com>
2022-12-05 14:07:31 +0900

    - Usersテーブルへカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_031736) do
+ActiveRecord::Schema.define(version: 2022_12_05_045546) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2022_12_05_031736) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "post_code"
+    t.text "address"
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
