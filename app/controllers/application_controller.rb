84103c4026d00aea3ddf62d8f05e0dd83a6ac89f
damisumida <gjptmda628@gmail.com>
2022-12-14 10:42:57 +0900

    - サインアップ、サインイン、ログアウト後の遷移先を指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(_resource)
+    books_path
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
