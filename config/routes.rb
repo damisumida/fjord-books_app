fbb36e5c38ca1c72a86301d1c4f57436c9e500f0
damisumida <gjptmda628@gmail.com>
2022-12-15 19:56:03 +0900

    - letter_opener_webを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   }
   get "users/show" => "users#show"
   get "users" => "users#index"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
