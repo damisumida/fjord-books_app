bdb294f1cd03c1c36a2c31cfdcb5499286177267
damisumida <gjptmda628@gmail.com>
2023-01-27 01:07:50 +0900

    フォロー解除を追加

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -4,4 +4,10 @@ class FollowController < ApplicationController
     user.following.create(follower_id: params[:user_id])
     redirect_to request.referer
   end
+
+  def destroy
+    follow = Follow.find_by(follower_id: params[:user_id], following_id: current_user.id)
+    follow.destroy
+    redirect_to request.referer
+  end
 end
