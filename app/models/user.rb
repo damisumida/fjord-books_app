9ef9afc4854248fa63051187bcc33bc0e1b1e1b8
damisumida <gjptmda628@gmail.com>
2023-02-02 20:23:00 +0900

    フォロー、フォロー解除ボタンの表示条件に以下を追加 - 自身のユーザの場合、表示しない - フォロー済みの場合フォローボタンを表示しない - 未フォローの場合、フォロー解除ボタンを表示しない Userモデルの不要なコメントを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,9 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  # フォローをした、されたの関係 activererationship
-  # フォロワーIDとユーザIDの関係
   has_many :follower, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
-  # フォローIDとユーザIDの関係
   has_many :following, class_name: "Follow", foreign_key: "following_id", dependent: :destroy
-  # foreign_keyはvaridateに変えても変わらない？
 
-  # 一覧画面で使う
-  # フォロー一覧
   has_many :followings, through: :following, source: :follower
-  # フォロワー一覧
   has_many :followers, through: :follower, source: :following
 end
