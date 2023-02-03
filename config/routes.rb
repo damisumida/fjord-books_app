320d523911135d1e6bad81ee6dd6325f701d896b
damisumida <gjptmda628@gmail.com>
2023-02-03 10:16:19 +0900

    フォロー一覧画面のURLを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show) do
-    resources :following, only: %i(index create destroy), controller: 'follow'
+    resources :following, only: %i(create destroy), controller: 'follow'
+    resources :followings, only: %i(index), to: 'follow#index'
     resources :followers, only: %i(index), to: 'follow#follower_index'
   end
 end
