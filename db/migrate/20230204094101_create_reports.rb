5d44f5ef71020e920c49ce80a4998b7ac2de85a7
damisumida <gjptmda628@gmail.com>
2023-02-04 18:56:15 +0900

    reportsテーブルを追加

--- db/migrate/20230204094101_create_reports.rb
+++ db/migrate/20230204094101_create_reports.rb
@@ -1,6 +1,8 @@
 class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
+      t.string :title, null: false
+      t.text :body_text
 
       t.timestamps
     end
