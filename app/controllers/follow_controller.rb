e00f0c221b86d474cecd4507e5c1a9b3336d494b
damisumida <gjptmda628@gmail.com>
2023-02-04 18:11:16 +0900

    使用していない json形式リクエストの場合jsonを返す処理を削除

--- app/controllers/follow_controller.rb
+++ app/controllers/follow_controller.rb
@@ -4,25 +4,17 @@ class FollowController < ApplicationController
   def create
     user = User.find(params[:user_id])
     follow = Follow.new(following_id: current_user.id, follower: user)
-
-    respond_to do |format|
-      if follow.save
-        format.html { redirect_to request.referer, notice: t('controllers.common.notice_create', name: Follow.model_name.human) }
-        format.json { render :show, status: :created, location: user }
-      else
-        format.html { redirect_to request.referer, notice: t('errors.template.header.one', model: Follow.model_name.human) }
-        format.json { render json: user.errors, status: :unprocessable_entity }
-      end
+    if follow.save
+      redirect_to request.referer, notice: t('controllers.common.notice_create', name: Follow.model_name.human)
+    else
+      redirect_to request.referer, notice: t('errors.template.header.one', model: Follow.model_name.human)
     end
   end
 
   def destroy
     follow = Follow.find_by(follower_id: params[:user_id], following_id: current_user.id)
     follow.destroy
-    respond_to do |format|
-      format.html { redirect_to request.referer, notice: t('controllers.common.notice_destroy', name: Follow.model_name.human) }
-      format.json { head :no_content }
-    end
+    redirect_to request.referer, notice: t('controllers.common.notice_destroy', name: Follow.model_name.human)
   end
 
   def follower_index
