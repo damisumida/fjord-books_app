f4314d337a68cc4c16357dcbd16d1bce6cb50319
damisumida <gjptmda628@gmail.com>
2023-02-13 20:19:29 +0900

    redirect_toの指定先を変更

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,16 +5,16 @@ class FollowsController < ApplicationController
     user = User.find(params[:user_id])
     follow = Follow.new(following_id: current_user.id, follower: user)
     if follow.save
-      redirect_to controller: :users, action: :show, notice: t('controllers.common.notice_create', name: Follow.model_name.human)
+      redirect_to user, notice: t('controllers.common.notice_create', name: Follow.model_name.human)
     else
-      redirect_to controller: :users, action: :show, notice: t('errors.template.header.one', model: Follow.model_name.human)
+      redirect_to user, notice: t('errors.template.header.one', model: Follow.model_name.human)
     end
   end
 
   def destroy
     follow = Follow.find_by(follower_id: params[:user_id], following_id: current_user.id)
     follow.destroy
-    redirect_to controller: :users, action: :show, notice: t('controllers.common.notice_destroy', name: Follow.model_name.human)
+    redirect_to User.find(params[:user_id]), action: :show, notice: t('controllers.common.notice_destroy', name: Follow.model_name.human)
   end
 
   def follower_index
