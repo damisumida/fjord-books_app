c16ffb5a35c34948afa36cb4d3ce4ed1342cadbb
damisumida <gjptmda628@gmail.com>
2023-02-16 09:39:00 +0900

    comments_controllerの不要な処理を削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,11 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: %i[destroy]
 
   def create
-    user = User.find(current_user.id)
     comment = Comment.new(comment_params)
-
-    comment.user = user
+    comment.user = current_user
     comment.content = @content
     comment.save
     redirect_to @content, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-  end
-
-  def update
-    # comment = Comment.find(comment_id)
-    # comment.update(comment_params)
   end
 
   def destroy
