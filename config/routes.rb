f1f5025a3542cc726718804a125b0ab94db8ae48
damisumida <gjptmda628@gmail.com>
2023-02-22 10:24:36 +0900

    表示分けを削除 reportのdestroy, updateに作成者チェックを追加 routesのresourceをresourcesに修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resource :comments, only: %i[create destroy], module: :books
+    resources :comments, only: %i[create destroy], module: :books
   end
   resources :reports do
-    resource :comments, only: %i[create destroy], module: :reports
+    resources :comments, only: %i[create destroy], module: :reports
   end
-  resource :comments, only: %i[update]
+  resources :comments, only: %i[update]
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
