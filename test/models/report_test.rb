a9dc175454197566c7d892cbc2299baf6671e72a
damisumida <gjptmda628@gmail.com>
2023-02-23 01:03:59 +0900

    ユーザとレポートのシステムテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    report = Report.new(title: 'hoge', content: 'fuga')
+    me = User.create!(email: 'me@example.com', password: 'passsword')
+    she = User.create!(email: 'she@example.com', password: 'passsword')
+
+    report.user = me
+    report.save
+
+    assert report.editable?(me)
+    assert_not report.editable?(she)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo.Bar'
+    assert_equal 'Foo.Bar', user.name_or_email
+  end
+
+  test '#follow_to_unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'passsword')
+    she = User.create!(email: 'she@example.com', password: 'passsword')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
 end
