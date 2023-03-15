a766264d203414385a43c5c8ae43691205add1c5
damisumida <gjptmda628@gmail.com>
2023-03-15 22:04:25 +0900

    内容が重複しているテストメソッドを削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,32 +3,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test '#name_or_email' do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
-
-    user.name = 'Foo.Bar'
-    assert_equal 'Foo.Bar', user.name_or_email
-  end
-
-  test '#follow_to_#following?' do
-    alice = users(:alice)
-    bob = users(:bob)
-
-    assert_not alice.following?(bob)
-    alice.follow(bob)
-    assert alice.following?(bob)
-  end
-
-  test '#followed_by?' do
-    alice = users(:alice)
-    bob = users(:bob)
-
-    assert_not alice.followed_by?(bob)
-    bob.follow(alice)
-    assert alice.followed_by?(bob)
-  end
-
   test 'follow/unfollow/following?/followed_by?' do
     alice = users(:alice)
     bob = users(:bob)
